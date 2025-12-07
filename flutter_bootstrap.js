c1a6571226a54bc1955ac1147b8ddadb2a608047
fazil202 <mfazii14563@gmail.com>
2025-12-07 06:41:24 -0800

    loading issues

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,7 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2615210099"
+    serviceWorkerVersion: "2615210099",
+    timeoutMillis: 10000
   }
 });
