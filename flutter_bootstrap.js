0b35533180472aa074bec783913cc4a92497467f
fazil202 <mfazii14563@gmail.com>
2025-12-10 09:29:05 -0800

    changes for new build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,7 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2615210099",
-    timeoutMillis: 10000
+    serviceWorkerVersion: "2215588757"
   }
 });
