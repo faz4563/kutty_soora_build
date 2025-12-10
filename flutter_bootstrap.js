8a13aaaf084bb97064f4078089357893b3d15dee
fazil202 <mfazii14563@gmail.com>
2025-12-10 10:00:14 -0800

    canel issue

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "909660390"
+    serviceWorkerVersion: "1101217540"
   }
 });
