206d4e81b30320f871bc31c689c80c20b6044d62
fazil202 <mfazii14563@gmail.com>
2026-01-03 22:57:41 -0800

    home page changes made for web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1074536155"
+    serviceWorkerVersion: "2110526987"
   }
 });
